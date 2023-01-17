8428b81c0c062332e1095774674bd44bf5daf98b
catmage <edruesnes@gmail.com>
2023-01-17 17:14:41 +0100

    first commit

---
+++ src/datas/plantList.js
@@ -0,0 +1,83 @@
+import monstera from '../assets/monstera.jpg'
+
+
+export const plantList = [
+    {
+        name: 'monstera',
+        category: 'classique',
+        id: '1ed',
+        isBestSale: true,
+        light: 2,
+        water: 3,
+        cover: monstera
+    },
+    {
+        name: 'ficus lyrata',
+        category: 'classique',
+        id: '2ab',
+        light: 3,
+        water: 1,
+        cover: monstera
+    },
+    {
+        name: 'pothos argenté',
+        category: 'classique',
+        id: '3sd',
+        light: 1,
+        water: 2,
+        cover: monstera
+        // isSpecialOffer: true
+    },
+    {
+        name: 'yucca',
+        category: 'classique',
+        id: '4kk',
+        light: 3,
+        water: 1,
+        cover: monstera
+    },
+    {
+        name: 'olivier',
+        category: 'extérieur',
+        id: '5pl',
+        light: 3,
+        water: 1,
+        cover: monstera
+        // isAvailable: false
+    },
+    {
+        name: 'géranium',
+        category: 'extérieur',
+        id: '6uo',
+        light: 2,
+        water: 2,
+        cover: monstera
+    },
+    {
+        name: 'basilique',
+        category: 'extérieur',
+        id: '7ie',
+        isBestSale: true,
+        light: 2,
+        water: 3,
+        cover: monstera
+        // isSpecialOffer: true
+    },
+    {
+        name: 'aloe',
+        category: 'plante grasse',
+        id: '8fp',
+        light: 2,
+        water: 1,
+        cover: monstera
+        // isSpecialOffer: true
+    },
+    {
+        name: 'succulente',
+        category: 'plante grasse',
+        id: '9vn',
+        light: 2,
+        water: 1,
+        cover: monstera
+    }
+]
